d482f3737e9b5e719bfd9c0bb76499bc70d3405e
GCaringi <giannicaringi94@gmail.com>
2022-06-10 15:21:20 +0200

    Add visible flags with vue-lang-flag

--- src/components/molecules/m-card.vue
+++ src/components/molecules/m-card.vue
@@ -3,21 +3,30 @@
     {{elem.title}}
     {{elem.original_title}}
     {{elem.original_language}}
+    <lang-flag :iso = elem.original_language squared = " true "/>
     {{elem.vote_average}}
   </div>
 </template>
 
 <script>
+
+import LangFlag from 'vue-lang-code-flags';
+
 export default {
     name: "cardItem",
     props: {
         elem: {
             type: Array,
         }
+    },
+    components: {
+        LangFlag,
     }
 }
 </script>
 
-<style>
-
+<style lang = "css">
+    .cs{
+        font-size: 20px
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/*----------------------
+  VUE LANG CODE FLAGS
+----------------------*/
+import LangFlag from 'vue-lang-code-flags';
+ 
+Vue.component('lang-flag', LangFlag);
+
 Vue.config.productionTip = false
 
 new Vue({
