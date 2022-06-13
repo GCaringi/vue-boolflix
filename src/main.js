3996d3d8e823f7738dad728a9f564fa3c9ae4e42
GCaringi <giannicaringi94@gmail.com>
2022-06-13 12:31:51 +0200

    Installed tailwindcss

--- src/main.js
+++ src/main.js
@@ -12,6 +12,10 @@ import LangFlag from 'vue-lang-code-flags';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
+/*--------------------
+      TAILWINDCSS
+---------------------*/
+import './index.css'
 
 Vue.component('lang-flag', LangFlag);
 
